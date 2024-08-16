6053c918c823228c2d36a081e760d0fa59faa9b1
esot0001 <esot0001@algonquinlive.com>
2024-08-16 18:03:26 -0400

    feat: adds FAQs header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import desktopBg from "@/assets/images/background-pattern-desktop.svg";
 import mobileBg from "@/assets/images/background-pattern-mobile.svg";
+import IconStar from "@/assets/images/icon-star.svg";
 
 export default function Home() {
   return (
     <main className="min-h-screen relative p-4 pb-10 bg-purple-100">
       Hello World!
       <BackgroundImage />
+      {/* max width on mobile screens is 320px and 768px on larger screens */}
+      <section className="relative bg-white mx-auto max-w-xs sm:max-w-4xl rounded-xl flex flex-col gap-4 mt-24 md:mt-35 p-5">
+        <h1 className="flex gap-3 items-center">
+          <Image src={IconStar} alt="star-icon" className="h-9 w-auto"></Image>
+          <span className="font-bold text-5xl">FAQs</span>
+        </h1>
+      </section>
     </main>
   );
 }
