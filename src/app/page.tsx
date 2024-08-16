d7d844d6c5b3978d677f9c74a41273c99f98689c
esot0001 <esot0001@algonquinlive.com>
2024-08-16 17:28:12 -0400

    feat: creates background image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,24 @@ export default function Home() {
   return (
     <main className="min-h-screen relative p-4 pb-10 bg-purple-100">
       Hello World!
-      <div className="w-full h-auto absolute top-0 left-0">
-        <Image
-          src={desktopBg}
-          alt="desktop-background"
-          className="w-full h-full object-cover hidden md:block"
-        ></Image>
-        <Image
-          src={mobileBg}
-          alt="mobile-background"
-          className="w-full h-full object-cover md:hidden"
-        ></Image>
-      </div>
+      <BackgroundImage />
     </main>
+  );
+}
+
+function BackgroundImage() {
+  return (
+    <div className="w-full h-auto absolute top-0 left-0">
+      <Image
+        src={desktopBg}
+        alt="desktop-background"
+        className="w-full h-full object-cover hidden md:block"
+      ></Image>
+      <Image
+        src={mobileBg}
+        alt="mobile-background"
+        className="w-full h-full object-cover md:hidden"
+      ></Image>
+    </div>
   );
 }
