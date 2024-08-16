9b977c6cf4034bb84fb9974e9822764ac90f2a9f
esot0001 <esot0001@algonquinlive.com>
2024-08-16 16:56:37 -0400

    feat: adds background color and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
+import desktopBg from "@/assets/images/background-pattern-desktop.svg";
 
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <main className="min-h-screen relative p-4 pb-10 bg-purple-100">
+      Hello World!
+      <div className="w-full h-auto absolute top-0 left-0">
+        <Image
+          src={desktopBg}
+          alt="desktop-background"
+          className="w-full h-full object-cover"
+        ></Image>
+      </div>
+    </main>
+  );
 }
