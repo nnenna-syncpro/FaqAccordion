51a9b7f61b70233a91c91d2e7e484e06012a646c
esot0001 <esot0001@algonquinlive.com>
2024-08-17 16:54:07 -0400

    feat: hides answer if accordion is closed

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -1,26 +1,35 @@
+"use client"; // useState only works in a Client Component marked with "use client". so they're Server Components by default.
 import * as React from "react";
 import Image from "next/image";
 import IconMinus from "@/assets/images/icon-minus.svg";
 import IconPlus from "@/assets/images/icon-plus.svg";
+import { useState } from "react";
 
 type Props = {};
 
 export default function Accordian({}: Props) {
+  const [isAccordionOpen, setAccordionOpen] = useState(false);
   return (
     <div className="flex flex-col gap-4 py-4">
       {/* question */}
       <div className="flex justify-between cursor-pointer font-bold text-xl md:text-2xl">
         <span>Question</span>
         <div>
-          <Image src={IconMinus} alt="minus-icon" className="h-6"></Image>
-          {/* <Image src={IconPlus} alt="plus-icon" className="h-6"></Image> */}
+          {isAccordionOpen ? (
+            <Image src={IconMinus} alt="minus-icon" className="h-6"></Image>
+          ) : (
+            <Image src={IconPlus} alt="plus-icon" className="h-6"></Image>
+          )}
         </div>
       </div>
-      {/* for the answer use backticks to avoid any text errors */}
-      <div>
-        <p className="text-base md:text-lg text-gray-500 pb-3">{`Answer`}</p>
-        <hr className="bg-gray-100" />
-      </div>
+      {/* if accordion is open then show answer */}
+      {isAccordionOpen && (
+        <div>
+          {/* for the answer use backticks to avoid any text errors */}
+          <p className="text-base md:text-lg text-gray-500 pb-3">{`Answer`}</p>
+          <hr className="bg-gray-100" />
+        </div>
+      )}
     </div>
   );
 }
