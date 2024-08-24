210854aabd362b04fcbe6ffa971ab7dd85a702ba
esot0001 <esot0001@algonquinlive.com>
2024-08-23 23:07:37 -0400

    adds route /faq and refactors homepage

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { Suspense, useMemo, useState } from "react";
+import Faq from "@/app/components/Faq/Faq";
+import Search from "@/app/components/Search";
+import { useSearchParams } from "next/navigation";
+import { faqs } from "@/data/data";
+import { BackgroundImage } from "@/app/background-image";
+
+export default function Faqs() {
+  //use state to manage all faqs
+  const [faqList] = useState(faqs);
+  //handle search term using search params
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("search") ?? "";
+
+  //filter FAQs list using search term. But only when faqs or search term changes
+  const filteredFaqList = useMemo(() => {
+    return faqList.filter((faq) => {
+      if (searchQuery !== null) {
+        return faq.question.toLowerCase().includes(searchQuery.toLowerCase());
+      } else {
+        return faqList;
+      }
+    });
+  }, [faqList, searchQuery]);
+
+  //create state for each accordion item and expand/collapse all
+  const [isOpen, setOpen] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  //check if any state changes to true
+  const isSomeOpen = isOpen.some((item) => item);
+
+  //handle toggle for all states including expand/collapse all
+  const toggleOpen = () => {
+    //if any state is true change it to false and vice versa
+    isSomeOpen
+      ? setOpen([false, false, false, false, false, false, false])
+      : setOpen([true, true, true, true, true, true, true]);
+  };
+
+  return (
+    <main className="min-h-screen relative p-4 pb-10 bg-purple-100">
+      <BackgroundImage />
+      <Suspense>
+        <Search />
+      </Suspense>
+      <Faq
+        handleToggle={toggleOpen}
+        isOpen={isSomeOpen}
+        faqs={filteredFaqList}
+        active={isOpen}
+        setOpen={setOpen}
+      />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,15 @@
-"use client";
-import { useMemo, useState } from "react";
-import Faq from "./components/Faq/Faq";
-import Search from "./components/Search";
-import { useSearchParams } from "next/navigation";
-import { faqs } from "@/data/data";
 import { BackgroundImage } from "./background-image";
+import Link from "next/link";
 
 export default function Home() {
-  //use state to manage all faqs
-  const [faqList] = useState(faqs);
-  //handle search term using search params
-  const searchParams = useSearchParams();
-  const searchQuery = searchParams.get("search") ?? "";
-
-  //filter FAQs list using search term. But only when faqs or search term changes
-  const filteredFaqList = useMemo(() => {
-    return faqList.filter((faq) => {
-      if (searchQuery !== null) {
-        return faq.question.toLowerCase().includes(searchQuery.toLowerCase());
-      } else {
-        return faqList;
-      }
-    });
-  }, [faqList, searchQuery]);
-
-  //create state for each accordion item and expand/collapse all
-  const [isOpen, setOpen] = useState([
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-    false,
-  ]);
-
-  //check if any state changes to true
-  const isSomeOpen = isOpen.some((item) => item);
-
-  //handle toggle for all states including expand/collapse all
-  const toggleOpen = () => {
-    //if any state is true change it to false and vice versa
-    isSomeOpen
-      ? setOpen([false, false, false, false, false, false, false])
-      : setOpen([true, true, true, true, true, true, true]);
-  };
-
   return (
     <main className="min-h-screen relative p-4 pb-10 bg-purple-100">
       <BackgroundImage />
-      <Search />
-      <Faq
-        handleToggle={toggleOpen}
-        isOpen={isSomeOpen}
-        faqs={filteredFaqList}
-        active={isOpen}
-        setOpen={setOpen}
-      />
+      <section className="relative bg-purple-100 mx-auto max-w-max rounded-full my-auto mt-20 md:mt-28 p-3">
+        <Link href="/faq" className="">
+          FAQs Link
+        </Link>
+      </section>
     </main>
   );
 }
