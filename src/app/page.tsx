1d0eebd847bd595723f53dddf4830147e8e2dc6a
esot0001 <esot0001@algonquinlive.com>
2024-08-23 21:55:20 -0400

    feat: refactors faq data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useMemo, useState } from "react";
 import Faq from "./components/Faq/Faq";
 import Search from "./components/Search";
 import { useSearchParams } from "next/navigation";
+import { faqs } from "@/data/data";
 
 export default function Home() {
   //use state to manage all faqs
@@ -80,21 +81,21 @@ function BackgroundImage() {
   );
 }
 
-const faqs = [
-  {
-    question: "What is Next.js?",
-    answer: "Next.js is a React framework for building web applications.",
-    id: 1,
-  },
-  {
-    question: "How does Tailwind CSS work?",
-    answer:
-      "Tailwind CSS is a utility-first CSS framework for rapidly building custom designs.",
-    id: 2,
-  },
-  {
-    question: "What is the purpose of getStaticProps?",
-    answer: "getStaticProps is used to fetch data at build time in Next.js.",
-    id: 3,
-  },
-];
+// const faqs = [
+//   {
+//     question: "What is Next.js?",
+//     answer: "Next.js is a React framework for building web applications.",
+//     id: 1,
+//   },
+//   {
+//     question: "How does Tailwind CSS work?",
+//     answer:
+//       "Tailwind CSS is a utility-first CSS framework for rapidly building custom designs.",
+//     id: 2,
+//   },
+//   {
+//     question: "What is the purpose of getStaticProps?",
+//     answer: "getStaticProps is used to fetch data at build time in Next.js.",
+//     id: 3,
+//   },
+// ];
---
+++ src/data/data.ts
@@ -0,0 +1,18 @@
+export const faqs = [
+  {
+    question: "What is Next.js?",
+    answer: "Next.js is a React framework for building web applications.",
+    id: 1,
+  },
+  {
+    question: "How does Tailwind CSS work?",
+    answer:
+      "Tailwind CSS is a utility-first CSS framework for rapidly building custom designs.",
+    id: 2,
+  },
+  {
+    question: "What is the purpose of getStaticProps?",
+    answer: "getStaticProps is used to fetch data at build time in Next.js.",
+    id: 3,
+  },
+];
